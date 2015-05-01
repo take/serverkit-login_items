c6f1ea0d86ef87daabe8af4dd04c932602d3a63a
Takehiro Adachi <takehiro0740@gmail.com>
2015-05-01 16:10:50 +0900

    Add spec as dev dependency

--- spec/serverkit/login_items_spec.rb
+++ spec/serverkit/login_items_spec.rb
@@ -4,8 +4,4 @@ describe Serverkit::LoginItems do
   it 'has a version number' do
     expect(Serverkit::LoginItems::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
